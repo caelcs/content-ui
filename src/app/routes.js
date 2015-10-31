47199cffa9a36a43c71846735e958b278321a1eb
caelwinner <adolfoecs@gmail.com>
2015-10-31 00:57:29 +0000

    #1: Fixing all the links on the menu

--- src/app/routes.js
+++ src/app/routes.js
@@ -3,13 +3,53 @@
     /**
      * Route configuration for the CopyshareContent module.
      */
-    angular.module('CopyshareContent').config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-        // For unmatched routes
+    angular
+        .module('CopyshareContent')
+        .config(
+            [
+                '$stateProvider',
+                 '$urlRouterProvider', 
+                 cpStateProviders
+            ]
+    );
+
+    function cpStateProviders ($stateProvider, $urlRouterProvider) {
         $urlRouterProvider.otherwise('/');
 
-        $stateProvider.state('intro', {
+        $stateProvider.state('index', {
             url: '/',
             templateUrl: '/components/dashboard/intro/intro.html'
         });
-    }]);
+
+        $stateProvider.state('intro', {
+            url: '/intro',
+            templateUrl: '/components/dashboard/intro/intro.html'
+        });
+
+        $stateProvider.state('services', {
+            url: '/services',
+            templateUrl: '/components/dashboard/services/services.html'
+        });
+
+        $stateProvider.state('about', {
+            url: '/about',
+            templateUrl: '/components/dashboard/about/about.html'
+        });
+
+        $stateProvider.state('portfolio', {
+            url: '/portfolio',
+            templateUrl: '/components/dashboard/portfolio/portfolio.html'
+        });
+
+        $stateProvider.state('team', {
+            url: '/team',
+            templateUrl: '/components/dashboard/team/team.html'
+        });
+
+        $stateProvider.state('contact', {
+            url: '/contact',
+            templateUrl: '/components/dashboard/contact/contact.html'
+        });
+    };
+
 }());
