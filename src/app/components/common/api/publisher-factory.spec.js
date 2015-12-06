23cd4aa413963a8f1150a3028588b4a3ee824e02
caelwinner <adolfoecs@gmail.com>
2015-12-06 21:07:25 +0000

    #10: removing backend mocking.

--- src/app/components/common/api/publisher-factory.spec.js
+++ src/app/components/common/api/publisher-factory.spec.js
@@ -1,17 +1,11 @@
 describe('Publisher Factory function', function() {
-  var Restangular, api, httpBackend, authRequestHandler;
+  var Restangular, api;
 
   beforeEach(module('restangular'));
   beforeEach(module('CopyshareContent.common.api.publisher'));
 
   beforeEach(inject(function(_Restangular_) {
   	Restangular = _Restangular_;
-  }));
-
-  beforeEach(inject(function(_$httpBackend_) {
-    httpBackend = _$httpBackend_;
-    httpBackend.verifyNoOutstandingExpectation();
-    httpBackend.verifyNoOutstandingRequest();
   }));
 
   beforeEach(inject(function(PublisherApi) {
@@ -22,15 +16,9 @@ describe('Publisher Factory function', function() {
     it('should get publisher by Access Token', function() {
     	spyOn(Restangular, 'one').and.callThrough();
 
-      // This will fail the spec if this http request does not happen.
-      httpBackend.whenGET('/publisher/current').respond({id:1, title: 'my title', body: 'my body'});
-
-    	var responseApi = api.getCurrentLogin();
+    	api.getCurrentLogin();
   
       expect(Restangular.one).toHaveBeenCalled();
-      expect(responseApi).toBeDefined();
-
-      httpBackend.flush();
     });
   });
 });
